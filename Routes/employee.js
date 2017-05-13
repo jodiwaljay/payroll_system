6a41c4611738446d23c6b955c89896670dbaf101
jodiwaljay <jodiwaljay@gmail.com>
2017-05-13 10:54:47 +0530

    Created routes and shifted database to mLab

---
+++ Routes/employee.js
@@ -0,0 +1,28 @@
+var uri = 'mongodb://jodiwal:strong1234@ds139781.mlab.com:39781/payroll_database';
+var express = require('express');
+var mongoose = require('mongoose');
+var router = express.Router();
+var Message = require('../models/message');
+
+mongoose.connect(uri);
+
+router.get('/', (req, res) => {
+  Message.find(function (err, Messages) {
+    if (err) return console.error(err);
+    res.send(Messages);
+  });
+});
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  var newMessage = new Message(req.body);
+  newMessage.save((err, doc) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(doc);
+    }
+  });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 'use strict'
 //first we import our dependencies…
 var express = require('express');
-var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
-var Message = require('./models/message');
 
-//and create our instances
+
 var app = express();
-var router = express.Router();
-//set our port to either a predetermined port number if you have set
-//it up, or 3001
+
+// route file for employee database handling
+var employeeRoute = require('./Routes/employee');
+
+//set our port to either a predetermined port number if you have set it up, or 3001
 var port = 3001;
-//now we should configure the API to use bodyParser and look for
+
 //JSON data in the request body
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -28,31 +28,11 @@ app.use(function(req, res, next) {
  res.setHeader('Cache-Control', 'no-cache');
  next();
 });
-//now we can set the route path & initialize the API
-router.get('/', (req, res) => {
-  Message.find(function (err, Messages) {
-    if (err) return console.error(err);
-    res.send(Messages);
-  });
-});
 
-router.post('/', (req, res) => {
-  console.log(req.body);
-  var newMessage = new Message(req.body);
-  newMessage.save((err, doc) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(doc);
-    }
-  });
-});
+//Sending all employee related database requests to employeeRoute defined above
+app.use('/database_handle', employeeRoute);
 
-//Use our router configuration when we call /api
-app.use('/database_handle', router);
 //starts the server and listens for requests
 app.listen(port, function() {
  console.log('api running on port ${port}');
 });
-
-mongoose.connect('mongodb://localhost/my');
